be597a5fd7615c0a0ac69b2c96e67b6ce66ca5dd
fengxianwei <coderwei1998@163.com>
2026-01-16 23:12:26 +0800

    1.新数据库操作文件

--- GetServer/mySqlDao.h
+++ GetServer/mySqlDao.h
@@ -1,9 +1,18 @@
 #pragma once
-// 数据库操作类
+#include <string>
+#include <memory>
+#include "mySqlPool.h"
 class mySqlDao
 {
-
-
-
+public:
+	mySqlDao();
+	~mySqlDao();
+	int RegUser(const std::string& name, const std::string& email, const std::string& pwd);
+	//int RegUserTransaction(const std::string& name, const std::string& email, const std::string& pwd, const std::string& icon);
+	bool CheckEmail(const std::string& name, const std::string& email);
+	bool UpdatePwd(const std::string& name, const std::string& newpwd);
+	bool CheckPwd(const std::string& email, const std::string& pwd, UserInfo& userInfo);
+	//bool TestProcedure(const std::string& email, int& uid, string& name);
+private:
+	std::unique_ptr<mySqlPool> pool_;
 };
-
